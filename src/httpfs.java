c016c909de962e162cd0baae37ebaec180979218
Aryan <aryan.saxena094@gmail.com>
2023-11-02 15:31:51 -0400

    first commit

---
+++ src/httpfs.java
@@ -0,0 +1,10 @@
+public class httpsv {
+    public static void main(String[] args) throws Exception {
+        // Scanner sc = new Scanner(System.in);
+        // String line = sc.nextLine();
+        String line = "httpfs -v -p 8080 -d /Users/ahmed/Desktop/CS/COMP445/Assignments/Assignment1/src";
+        String[] arguments = line.split(" ");
+        String command = arguments[0];
+
+    }
+}
