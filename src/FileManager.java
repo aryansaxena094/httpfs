22369e78bee9ae922aae7cd9a7c7c399dc7c6d54
Aryan <aryan.saxena094@gmail.com>
2023-11-05 20:47:44 -0500

    file-locking

--- src/FileManager.java
+++ src/FileManager.java
@@ -2,34 +2,60 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.stream.Collectors;
 
 public class FileManager {
     private final Path baseDirectory;
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
 
     FileManager(String directory) {
         this.baseDirectory = Path.of(directory);
     }
 
-    public synchronized List<String> listFiles() throws IOException {
-        return Files.list(baseDirectory).map(Path::getFileName).map(Path::toString).collect(Collectors.toList());
+    public List<String> listFiles() throws IOException {
+        readWriteLock.readLock().lock();
+        try {
+            return Files.list(baseDirectory)
+                        .map(Path::getFileName)
+                        .map(Path::toString)
+                        .collect(Collectors.toList());
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
     }
 
-    public synchronized String readFile(String fileName) throws IOException {
-        Path filePath = baseDirectory.resolve(fileName);
-        validatePath(filePath);
-        return new String(Files.readAllBytes(filePath));
+    public String readFile(String fileName) throws IOException {
+        readWriteLock.readLock().lock();
+        try {
+            Path filePath = baseDirectory.resolve(fileName);
+            validatePath(filePath);
+            return new String(Files.readAllBytes(filePath));
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
     }
 
-    public synchronized void writeFile(String fileName, String content) throws IOException {
-        Path filePath = baseDirectory.resolve(fileName);
-        validatePath(filePath);
-        Files.write(filePath, content.getBytes());
+    public void writeFile(String fileName, String content) throws IOException {
+        readWriteLock.writeLock().lock();
+        try {
+            Path filePath = baseDirectory.resolve(fileName);
+            validatePath(filePath);
+            Files.write(filePath, content.getBytes());
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
     }
 
-    public synchronized boolean fileExists(String fileName) {
-        Path filePath = baseDirectory.resolve(fileName);
-        return Files.exists(filePath);
+    public boolean fileExists(String fileName) {
+        readWriteLock.readLock().lock();
+        try {
+            Path filePath = baseDirectory.resolve(fileName);
+            return Files.exists(filePath);
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
     }
 
     private void validatePath(Path filePath) {
